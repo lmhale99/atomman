2660d004068080ba5333e4d2d940ac8f70884f4b
Lucas Hale <lucas.hale@nist.gov>
2016-01-26 13:08:02 -0500

    new lammps.log_extract to DataModelDict instead of a list

---
+++ atomman/lammps/log_extract.py
@@ -0,0 +1,67 @@
+
+#External library imports
+import numpy as np
+
+#atomman imports
+from atomman.models import DataModelDict
+
+def log_extract(log_info):
+#Parse a LAMMPS screen output/log file into a DataModelDict
+    
+    #Create DataModelDict root
+    log_dict = DataModelDict()
+    log_dict['LAMMPS-log-thermo-data'] = DataModelDict()
+    log_dict['LAMMPS-log-thermo-data']['simulation'] = []
+    
+    #Initialize necessary parameters
+    thermoread = False
+    thermolist = None
+    
+    #Convert string to list if needed
+    if isinstance(log_info, (str, unicode)):
+        log_info = log_info.split('\n')    
+    
+    #for all lines in file/output
+    for line in log_info:
+        terms = line.split()
+     
+        #If the line has terms
+        if len(terms)>0:
+ 
+            #thermoread indicates time to read data
+            if thermoread:
+                
+                #save values in list if they are numbers
+                if terms[0].isdigit():
+                    if thermolist is None:
+                        thermolist = np.array([terms], dtype=float)
+                    else:
+                        thermolist = np.append(thermolist, np.array([terms], dtype=float), axis=0)                    
+                
+                #if not values, transform to dictionary
+                else:
+                    thermo = DataModelDict()
+                    for i in xrange(len(headers)):
+                        thermoval = thermolist[:,i]
+                        if np.allclose(thermoval, np.asarray(thermoval, dtype=int)):
+                            thermoval = np.asarray(thermoval, dtype=int)
+                        thermo[headers[i]] = list(thermoval)
+                        
+                    #add dictionary to simulation list
+                    log_dict['LAMMPS-log-thermo-data']['simulation'].append(DataModelDict([('thermo',thermo)]))                    
+                    
+                    #reset thermoread and thermolist
+                    thermolist = None
+                    thermoread = False    
+                    
+                    
+            #If the line starts with Step, then save headers and set thermoread
+            elif terms[0] == 'Step':
+                headers = terms
+                thermoread = True
+                   
+    #downsize simulation if it only has one term
+    if len(log_dict['LAMMPS-log-thermo-data']['simulation']) == 1:
+        log_dict['LAMMPS-log-thermo-data']['simulation'] = log_dict['LAMMPS-log-thermo-data']['simulation'][0]
+    
+    return log_dict     
