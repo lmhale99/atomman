1a2a1f1f072145a48cdd585dc4f091c858303b79
Lucas Hale <lucas.hale@nist.gov>
2016-06-30 10:21:15 -0400

    made cif loading optional: works if diffpy.Structure is installed

--- atomman/convert/cif.py
+++ atomman/convert/cif.py
@@ -1,9 +1,16 @@
-import diffpy.Structure 
 import atomman as am
 import numpy as np
 
+try:
+    import diffpy.Structure
+    has_diffpy = True
+except:
+    has_diffpy = False
 
 def load(cif):
+    """Reads in a CIF crystal file and returns an atomman.System and list of elements."""
+    
+    assert has_diffpy, 'diffpy.Structure not imported'
     
     dps = diffpy.Structure.structure.Structure()
     
@@ -40,13 +47,3 @@ def load(cif):
                  gamma=dps.lattice.gamma)
     
     return am.System(atoms=atoms, box=box, pbc=(True,True,True), scale=True), list(elements)
-   
-    
-if __name__ == '__main__':
-    import glob
-    import os
-    for fname in glob.iglob('E:\\crystal-structures\\oqmd\\cif\\*.cif'):
-        with open(fname) as f:
-            elements, system = load(f)
-    
-        print os.path.basename(fname), system.box.a, system.box.b, system.box.c, system.box.alpha, system.box.beta, system.box.gamma
