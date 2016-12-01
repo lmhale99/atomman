7cd6fe05b895170b31ee7003c98c7f4eab3b362b
Lucas Hale <lucas.hale@nist.gov>
2016-12-01 11:25:26 -0500

    Updated version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
     
 
 setup(name='atomman',
-      version='1.0.1',
+      version='1.1.0',
       description='Atomistic Manipulation Toolkit',
       long_description=readme(),
       classifiers=[
