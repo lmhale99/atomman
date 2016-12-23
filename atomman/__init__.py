b61d85c24d539d55a57c06d1b2247d31479b860d
Lucas Hale <lucas.hale@nist.gov>
2016-12-23 14:07:33 -0500

    changed local imports to be relative

--- atomman/__init__.py
+++ atomman/__init__.py
@@ -1,9 +1,9 @@
-from core import *
-import unitconvert
-import defect
-import lammps
-import tools
-import convert
-import plot
+from .core import *
+from . import unitconvert
+from . import defect
+from . import lammps
+from . import tools
+from . import convert
+from . import plot
 
 unitconvert.reset_units(length = 'angstrom', mass = 'amu', energy='eV', charge='e')
