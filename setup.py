261f2b01896c2cc4b1c0f4e7ee7dc58152b3c000
Lucas Hale <lucas.hale@nist.gov>
2017-07-12 10:12:29 -0400

    updated docs and version info

--- setup.py
+++ setup.py
@@ -1,27 +1,36 @@
+import os
 from setuptools import setup, find_packages
 
-def readme():
+def getversion():
+    with open(os.path.join('atomman', 'VERSION')) as version_file:
+        version = version_file.read().strip()
+
+def getreadme():
     with open('README.rst') as f:
         return f.read()
     
-
-setup(name='atomman',
-      version='1.1.2',
-      description='Atomistic Manipulation Toolkit',
-      long_description=readme(),
-      classifiers=[
+setup(name = 'atomman',
+      version = getversion(),
+      description = 'Atomistic Manipulation Toolkit',
+      long_description = getreadme(),
+      classifiers = [
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
         'Topic :: Scientific/Engineering :: Physics'
       ],
-      keywords=['atom', 'atomic', 'atomistic', 'molecular dynamics'], 
-      url='https://github.com/usnistgov/atomman',
-      author='Lucas Hale',
-      author_email='lucas.hale@nist.gov',
-      packages=find_packages(),
-      install_requires=[
+      keywords = [
+        'atom', 
+        'atomic', 
+        'atomistic', 
+        'molecular dynamics'
+      ], 
+      url = 'https://github.com/usnistgov/atomman',
+      author = 'Lucas Hale',
+      author_email = 'lucas.hale@nist.gov',
+      packages = find_packages(),
+      install_requires = [
         'xmltodict',
         'DataModelDict',
         'numpy', 
@@ -30,5 +39,5 @@ setup(name='atomman',
         'pandas',
         'numericalunits'
       ],
-      include_package_data=True,
-      zip_safe=False)
+      include_package_data = True,
+      zip_safe = False)
