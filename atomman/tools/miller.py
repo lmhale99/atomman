05aa4de614dd2e2782029a850ce4e3e01eebecd1
Lucas Hale <lucas.hale@nist.gov>
2020-04-07 15:00:59 -0400

    plane_crystal_to_cartesian function added

--- atomman/tools/miller.py
+++ atomman/tools/miller.py
@@ -6,6 +6,7 @@ import numpy as np
 from .crystalsystem import ishexagonal
 
 __all__ = ['plane3to4', 'plane4to3', 'vector3to4', 'vector4to3',
+           'plane_crystal_to_cartesian',
            'vector_crystal_to_cartesian',
            'vector_primitive_to_conventional', 
            'vector_conventional_to_primitive']
@@ -175,7 +176,7 @@ def vector_crystal_to_cartesian(indices, box):
     indices = np.array(indices)
     
     # Convert 4-term Miller-Bravais to standard 3-term indices
-    if indices.shape[-1] == 4:
+    if indices.shape[-1] == 4: # pylint: disable=unsubscriptable-object
         if not ishexagonal(box):
             raise ValueError('Hexagonal indices given with non-hexagonal box')
         indices = vector4to3(indices)
@@ -306,3 +307,102 @@ def vector_conventional_to_primitive(indices, setting='p'):
         raise ValueError('Unknown lattice setting. Allowed values are: p, a, b, c, i, and f')
     
     return np.inner(indices, lat.T)
+
+def plane_crystal_to_cartesian(indices, box):
+    """
+    Converts crystal planar indices to Cartesian plane normal vectors relative
+    to a given lattice box.  Note: the algorithm used requires that the planar
+    indices be integers.
+    
+    Parameters
+    ----------
+    indices : np.ndarray
+        (3,) array of [hkl] Miller crystallographic indices or 
+        (4,) array of [hkil] Miller-Bravais crystallographic indices.
+    box : atomman.Box
+        Box that defines the lattice cell vectors to use. 
+   
+    Returns
+    -------
+    np.ndarray of float
+        (..., 3) array of Cartesian vectors corresponding to plane normals.
+        
+    Raises
+    ------
+    ValueError
+        If indices dimensions are not (..., 3) or (..., 4), or if
+        hexagonal indices given with non-hexagonal box.
+    """
+    # Check indices values
+    indices = np.asarray(indices)
+    
+    # Convert hkil to hkl
+    if indices.shape == (4,):
+        if ishexagonal(box):
+            indices = plane4to3(indices)
+        else:
+            raise ValueError('Hexagonal indices given with non-hexagonal box')
+    if indices.shape != (3,):
+        raise ValueError('Invalid index dimensions')
+    
+    if np.allclose(indices, np.asarray(indices, dtype=int)):
+        indices = np.asarray(indices, dtype=int)
+    else:
+        raise ValueError('Indices must be integers')
+    
+    # Find two in-plane box vectors
+    if indices[0] != 0:
+        if indices[1] != 0:
+            if indices[2] != 0:
+                # indices solution
+                m = np.lcm.reduce([indices[0], indices[1], indices[2]]) # pylint: disable=no-member
+                s = np.sign(indices[0] * indices[1] * indices[2])
+                a_uvw = np.array([-m / indices[0], m / indices[1], 0], dtype=int)
+                b_uvw = np.array([-m / indices[0], 0, m / indices[2]], dtype=int)
+            else:
+                # hk0 solution
+                m = np.lcm(indices[0], indices[1]) # pylint: disable=no-member
+                s = np.sign(indices[0] * indices[1])
+                a_uvw = np.array([-m / indices[0], m / indices[1], 0], dtype=int)
+                b_uvw = np.array([0, 0, 1], dtype=int)
+        else:
+            if indices[2] != 0:
+                # h0l solution
+                m = np.lcm(indices[0], indices[2]) # pylint: disable=no-member
+                s = np.sign(indices[0] * indices[2])
+                a_uvw = np.array([m / indices[0], 0, -m / indices[2]], dtype=int)
+                b_uvw = np.array([0, 1, 0], dtype=int)
+            else:
+                # h00 solution
+                m = 1
+                s = np.sign(indices[0])
+                a_uvw = np.array([0, 1, 0], dtype=int)
+                b_uvw = np.array([0, 0, 1], dtype=int)
+    elif indices[1] != 0:
+        if indices[2] != 0:
+            # 0kl solution
+            m = np.lcm(indices[1], indices[2]) # pylint: disable=no-member
+            s = np.sign(indices[1] * indices[2])
+            a_uvw = np.array([0, -m / indices[1], m / indices[2]], dtype=int)
+            b_uvw = np.array([1, 0, 0], dtype=int)
+        else:    
+            # 0k0 solution
+            m = 1
+            s = np.sign(indices[1])
+            a_uvw = np.array([0, 0, 1], dtype=int)
+            b_uvw = np.array([1, 0, 0], dtype=int)
+    elif indices[2] != 0:
+        # 00l solution
+        m = 1
+        s = np.sign(indices[2])
+        a_uvw = np.array([1, 0, 0], dtype=int)
+        b_uvw = np.array([0, 1, 0], dtype=int)
+    else:
+        raise ValueError('indices cannot be all zeros')
+    
+    # Compute Cartesian plane normal
+    planenormal = s * np.cross(np.inner(a_uvw, box.vects.T),
+                               np.inner(b_uvw, box.vects.T))
+
+    # Return the unit vector normal
+    return planenormal / np.linalg.norm(planenormal)
