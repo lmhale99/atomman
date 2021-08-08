14a7889fd6cad1734882f74ddaeb120751de1723
Lucas Hale <lucas.hale@nist.gov>
2021-08-08 10:27:39 -0400

    Automate more doc building components

--- doc/copytutorial.py
+++ doc/copytutorial.py
@@ -1,14 +1,24 @@
-from __future__ import print_function, unicode_literals
+from pathlib import Path
 import os
 import glob
 
-for oldpath in glob.iglob(os.path.join('tutorial', '*.ipynb')):
-    
+notebookdir = Path('tutorial')
+docdir = Path('source', 'tutorial')
+
+# Delete old Notebooks from the source folder
+for notebook in docdir.glob('*.ipynb'):
+    notebook.unlink()
+
+pagenames = []
+
+# Copy new Notebooks from tutorial to source
+for notebook in notebookdir.glob('*.ipynb'):
+    pagenames.append(notebook.stem)
     # Read in Notebook content
-    with open(oldpath) as f:
+    with open(notebook) as f:
         content = f.read()
     
-    # Change all links to Notebooks to html links
+    # Change all links from .ipynb to .html
     for i in range(100):
         try:
             endindex = content.index('.ipynb)')+6
@@ -21,8 +31,43 @@ for oldpath in glob.iglob(os.path.join('tutorial', '*.ipynb')):
             content = content.replace(oldlink, newlink)
     
     # Save to source directory
-    oldname = os.path.basename(oldpath)
+    oldname = notebook.name
     newname = oldname.replace(' ', '_')
-    newpath = os.path.join('source', 'tutorial', newname)
+    newpath = Path(docdir, newname)
     with open(newpath, 'w') as f:
         f.write(content)
+
+# Define sortkey for page numbers
+def sortkey(fname):
+    """
+    Sorts filenames based on numbers at the beginning of the file with up to
+    two subsets, i.e. #., #.#., or #.#.#.
+    """
+    numbers = fname.split(' ')[0].split('.')
+    val = int(numbers[0]) * 10000
+    try:
+        val += int(numbers[1]) * 100
+    except:
+        pass
+    else:
+        try:
+            val += int(numbers[2])
+        except:
+            pass
+    return val
+
+# Create index.rst header
+indexlines = [
+    "Tutorials",
+    "=========",
+    '',
+    '.. toctree::',
+    '   :maxdepth: 2',
+    '   ']
+
+# Build lines for each Notebook
+for name in sorted(pagenames, key=sortkey):
+    indexlines.append(f'   {name} <{name.replace(" ", "_")}>')
+
+with open(Path(docdir, 'index.rst'), 'w', encoding='UTF-8') as f:
+    f.write('\n'.join(indexlines))
