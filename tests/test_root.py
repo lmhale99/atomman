e7deda11b4bf56012182cdb2c75959bbfe9d3af2
lan496 <kohei19950508@gmail.com>
2022-01-11 08:32:32 +0000

    Not to use am.rootdir

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,22 +1,10 @@
 # coding: utf-8
 
 # Standard Python libraries
-import os
-
-# https://docs.pytest.org/en/latest/
-import pytest
-
-# http://www.numpy.org/
-import numpy as np
+from importlib import resources
 
 import atomman as am
 
-def test_rootdir():
-    assert os.path.isdir(am.rootdir)
-
 def test_version():
-    assert os.path.isfile(os.path.join(os.path.join(am.rootdir, 'VERSION')))
-    try:
-        print('Testing atomman version', am.__version__)
-    except:
-        assert False
+    assert resources.is_resource('atomman', 'VERSION')
+    print('Testing atomman version', am.__version__)
