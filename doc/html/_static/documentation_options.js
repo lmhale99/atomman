1d3fabcf1acca4dfc4579a51124c986ee628d4e5
Lucas Hale <lucas.hale@nist.gov>
2022-03-31 18:45:53 -0400

    updated docs

--- doc/html/_static/documentation_options.js
+++ doc/html/_static/documentation_options.js
@@ -1,9 +1,11 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.0',
+    VERSION: '1.4.4',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: false,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
