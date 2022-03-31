06c5a2ed652195f30a3489bee8b287b315df5d2f
Lucas Hale <lucas.hale@nist.gov>
2022-03-31 18:56:34 -0400

    updated setup for new potentials version

--- setup.py
+++ setup.py
@@ -6,35 +6,35 @@ try:
     from Cython.Build import cythonize
 except:
     USE_CYTHON = False
-    ext = '.c'
+    EXT = '.c'
 else:
     USE_CYTHON = True
-    ext = '.pyx'
+    EXT = '.pyx'
 
-extensions = [Extension("*", ["atomman/core/*" + ext]),
-              Extension("*", ["atomman/defect/*" + ext])]
+extensions = [Extension("*", ["atomman/core/*" + EXT]),
+              Extension("*", ["atomman/defect/*" + EXT])]
 
 if USE_CYTHON:
-    
     extensions = cythonize(extensions)
 
 def getversion():
     """Fetches version information from VERSION file"""
-    with open(os.path.join('atomman', 'VERSION')) as version_file:
+    with open(os.path.join('atomman', 'VERSION'), encoding='UTF-8') as version_file:
         version = version_file.read().strip()
     return version
 
 def getreadme():
     """Fetches description from README.rst file"""
-    with open('README.rst') as f:
+    with open('README.rst', encoding='UTF-8') as f:
         return f.read()
 
-setup(name = 'atomman',
-      version = getversion(),
-      description = 'Atomistic Manipulation Toolkit',
-      long_description = getreadme(),
-      ext_modules = extensions,
-      classifiers = [
+setup(
+    name = 'atomman',
+    version = getversion(),
+    description = 'Atomistic Manipulation Toolkit',
+    long_description = getreadme(),
+    ext_modules = extensions,
+    classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
         'Natural Language :: English',
@@ -42,29 +42,30 @@ setup(name = 'atomman',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Topic :: Scientific/Engineering :: Physics'
-      ],
-      keywords = [
-        'atom', 
-        'atomic', 
-        'atomistic', 
+    ],
+    keywords = [
+        'atom',
+        'atomic',
+        'atomistic',
         'molecular dynamics'
-      ], 
-      url = 'https://github.com/usnistgov/atomman/',
-      author = 'Lucas Hale',
-      author_email = 'lucas.hale@nist.gov',
-      packages = find_packages(),
-      install_requires = [
+    ],
+    url = 'https://github.com/usnistgov/atomman/',
+    author = 'Lucas Hale',
+    author_email = 'lucas.hale@nist.gov',
+    packages = find_packages(),
+    install_requires = [
         'xmltodict',
         'DataModelDict',
         'numericalunits',
-        'numpy>=1.15', 
+        'numpy>=1.15',
         'matplotlib',
         'scipy',
         'pandas',
         'cython',
         'requests',
         'toolz',
-        'potentials==0.3.2'
-      ],
-      include_package_data = True,
-      zip_safe = False)
+        'potentials==0.3.3'
+    ],
+    include_package_data = True,
+    zip_safe = False
+)
