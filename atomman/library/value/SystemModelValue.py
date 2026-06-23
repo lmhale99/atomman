72cd2e1656977a6cad0f8837291b3edc4eaa8469
Lucas Hale <lucas.hale@nist.gov>
2026-06-23 12:23:05 -0400

    Added prop_unit option

--- atomman/library/value/SystemModelValue.py
+++ atomman/library/value/SystemModelValue.py
@@ -13,6 +13,70 @@ from yabadaba.record import Record
 
 class SystemModelValue(Value):
     
+    def __init__(self,
+                 name: str,
+                 record,
+                 defaultvalue: Optional[Any] = None,
+                 valuerequired: bool = False,
+                 allowedvalues: Optional[tuple] = None,
+                 metadatakey: Union[str, bool, None] = None,
+                 metadataparent: Optional[str] = None,
+                 modelpath: Optional[str] = None,
+                 description: Optional[str] = None,
+                 prop_unit: Optional[dict] = None,):
+        """
+        Initialize a Value object.
+
+        Parameters
+        ----------
+        name : str
+            The name of the parameter.  This should correspond to the name of
+            the associated class attribute.
+        record : Record
+            The Record object that the Parameter is used with.
+        defaultvalue : any or None, optional
+            The default value to use for the property.  The default value of
+            None indicates that there is no default value.
+        valuerequired: bool
+            Indicates if a value must be given for the property.  If True, then
+            checks will be performed that a value is assigned to the property.
+        allowedvalues : tuple or None, optional
+            A list/tuple of values that the parameter is restricted to have.
+            Setting this to None (default) indicates any value is allowed.
+        metadatakey: str, bool or None, optional
+            The key name to use for the property when constructing the record
+            metadata dict.  If set to None (default) then name will be used for
+            metadatakey.  If set to False then the parameter will not be
+            included in the metadata dict.
+        metadataparent: str or None, optional
+            If given, then this indicates that the metadatakey is actually an
+            element of a dict in metadata with this name.  This allows for limited
+            support for metadata having embedded dicts.
+        modelpath: str, optional
+            The period-delimited path after the record root element for
+            where the parameter will be found in the built data model.  If set
+            to None (default) then name will be used for modelpath.
+        description: str or None, optional
+            A short description for the value.  If not given, then the record name
+            will be used.
+        prop_unit : dict, optional
+        dictionary where the keys are the property keys to include, and
+        the values are units to use. If neither unit nor prop_units given,
+        pos will be given in Angstroms and all other values will not be
+        converted.
+        """
+        self.__prop_unit = prop_unit
+
+        super().__init__(name, record, defaultvalue=defaultvalue,
+                         valuerequired=valuerequired, allowedvalues=allowedvalues,
+                         metadatakey=metadatakey, metadataparent=metadataparent,
+                         modelpath=modelpath, description=description)
+
+    @property
+    def prop_unit(self) -> Optional[dict]:
+        """dict or None: The units to use for property values when building the model"""
+        return self.__prop_unit
+
     @property
     def loadkey(self) -> str:
         """str: The last path name where the content """
@@ -35,7 +99,7 @@ class SystemModelValue(Value):
 
     def build_model_value(self):
         """Function to modify how values are represented in the model"""
-        return self.value.dump('system_model')[self.loadkey]
+        return self.value.dump('system_model', prop_unit=self.__prop_unit)[self.loadkey]
          
     def load_model_value(self, val):
         return DM([(self.loadkey, val)])
